0803cb7370d77f9c4202cc0d93989abc08dda7b8
akshayjith <akshayjithadoor@gmail.com>
2025-12-20 11:05:35 +0530

    Initial commit - Django blog project

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
-# Create your views here.
+
+def post_list(request):
+    posts = BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
